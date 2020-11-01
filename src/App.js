8dc982194f572e4f6edda1962f1374c93f57d140
kasapvictor <kasap.victor@gmail.com>
2020-11-01 08:48:44 +0300

    winner calculate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from './components/Board';
+import { calculateWinner } from "./helpers";
 import './styles/root.scss';
 
 const App = () => {
+    const [ board, setBoard ] = useState ( Array ( 9 ).fill ( null ) );
+    const [ isXNext, setIsXNext ] = useState ( false );
+    const [ counter, setCounter ] = useState (0);
+    const winner = calculateWinner(board);
+    const message = winner ? `Winner is ${winner}` : `Next player is ${!isXNext ? 'x' : 'o'}`;
+
+    const handleSquareClick = ( position ) => {
+        if ( board[position] || winner ) {
+            return
+        }
+
+        setBoard ( ( prev ) => {
+            return prev.map ( ( square, index ) => {
+                if ( index === position ) {
+                    return isXNext ? 'x' : 'o';
+                }
+                return square;
+            } );
+        } );
+
+        setIsXNext ( ( prev ) => !prev );
+
+        // count moves
+        setCounter ( (prev) => prev + 1);
+    }
+
     return (
         <div className="app">
             <h1>TIC TAC TOE</h1>
-
-            <Board/>
+            <div> Moves: {counter} / {message}</div>
+            <Board board={board} handleSquareClick={handleSquareClick}/>
         </div>
     )
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,51 +1,31 @@
 import React, { useState } from 'react';
 import Square from './Square';
 
-const Board = () => {
+const Board = ({board, handleSquareClick}) => {
 
-	const [ board, setBoard ] = useState ( Array ( 9 ).fill ( null ) );
-	const [isXNext, setIsXNext] = useState(false);
-
-	const handleSquareClick = (position) => {
-		if (board[position]) { return }
-
-		setBoard( (prev) => {
-			return prev.map((square, pos) => {
-				if (pos === position) {
-					return isXNext ? 'x' : 'o';
-				}
-				return square;
-			});
-		} );
-		setIsXNext( (prev) => !prev)
-	}
-
-	const renderSquare = (position) => {
-		return <Square value={ board[position] } onClick={ () => handleSquareClick(position) }/>;
+	const renderSquare = ( position ) => {
+		return <Square value={ board[position] } onClick={ () => handleSquareClick ( position ) }/>;
 	}
 
 	return (
 		<div className="board">
-
 			<div className="board-row">
-				{ renderSquare(0) }
-				{ renderSquare(1) }
-				{ renderSquare(2) }
+				{ renderSquare ( 0 ) }
+				{ renderSquare ( 1 ) }
+				{ renderSquare ( 2 ) }
 			</div>
 
 			<div className="board-row">
-				{ renderSquare(4) }
-				{ renderSquare(5) }
-				{ renderSquare(6) }
+				{ renderSquare ( 3 ) }
+				{ renderSquare ( 4 ) }
+				{ renderSquare ( 5 ) }
 			</div>
 
 			<div className="board-row">
-				{ renderSquare(7) }
-				{ renderSquare(8) }
-				{ renderSquare(9) }
+				{ renderSquare ( 6 ) }
+				{ renderSquare ( 7 ) }
+				{ renderSquare ( 8 ) }
 			</div>
-
-
 		</div>
 	);
 }
